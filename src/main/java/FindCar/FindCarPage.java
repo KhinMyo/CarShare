ee28efea8692a24384cfa323a5429a3b2f44f642
DELL <DELL@DESKTOP-OUG61TN>
2025-05-18 11:44:25 +0630

    -- Find Car fixed

--- src/main/java/FindCar/FindCarPage.java
+++ src/main/java/FindCar/FindCarPage.java
@@ -13,7 +13,7 @@ public class FindCarPage {
 	}
 
 	public void clickPickLocation() throws InterruptedException {
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		driver.findElement(By.cssSelector("div.select__dropdown-indicator")).click();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//*[text()='Bagan Airport , Bagan']")).click();
@@ -33,9 +33,8 @@ public class FindCarPage {
 		driver.findElement(By.xpath("//input[@name='return_date']")).click();
 		Thread.sleep(5000);
 //		driver.findElement(By.xpath("//*[local-name()='svg' and @xmlns='http://www.w3.org/2000/svg']")).click();
-		driver.findElement(By.xpath("//*[local-name()='svg' and @viewBox='0 0 17 17']" + "/*[local-name()='path' and "
-				+ "@d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z']"))
-				.click();
+		driver.findElement(By.xpath("/html[1]/body[1]/div[4]/div[1]/span[2]/*[name()='svg'][1]")).click();
+		
 		driver.findElement(By.xpath("//div[@class='dayContainer']//*[@aria-label= '" + returnDate + "']")).click();
 	}
 
@@ -44,7 +43,6 @@ public class FindCarPage {
 	}
 
 	public void clickFindCarBtn() {
-//		driver.findElement(By.xpath("//button[@id = 'SimulateButton' and @type = 'submit']")).click();
 		driver.findElement(By.id("SimulateButton")).click();
 	}
 }
--- src/test/java/carshare/TestCase.java
+++ src/test/java/carshare/TestCase.java
@@ -35,61 +35,62 @@ public class TestCase extends BaseTest {
 
 		about.clickAboutUs();
 		String title = about.getTitle();
-		
+
 		Assert.assertEquals(title, "About Us");
 	}
-	
+
 	@Test
 	public void WhyCarShare() throws InterruptedException {
 		CarSharePage about = new CarSharePage(driver);
 
 		about.clickWhyCarShare();
 		String title = about.getTitle();
-		
+
 		Assert.assertEquals(title, "Why Yoma Car Share?");
 	}
-	
+
 	@Test
 	public void HowCarShareWork() throws InterruptedException {
 		HowCarShareWorkPage about = new HowCarShareWorkPage(driver);
 
 		about.clickHowCarShareWork();
 		String title = about.getTitle();
-		
+
 		Assert.assertEquals(title, "How Yoma Car Share Works");
 	}
-	
+
 	@Test
 	public void SelfDrive() throws InterruptedException {
 		SelfDrivePage selfDrive = new SelfDrivePage(driver);
 
 		selfDrive.clickSelfDrive();
 		String title = selfDrive.getTitle();
-		
+
 		Assert.assertEquals(title, "Self Drive Tips");
 	}
 
-//	@Test
-//	public void FindCar() throws InterruptedException {
-//		FindCarPage onboard = new FindCarPage(driver);
-//
-//		LocalDate today = LocalDate.now().plusDays(1);
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
-//		String todaysDate = today.format(formatter);
-//
-//		LocalDate nextMonth = LocalDate.now().plusMonths(1);
-//		String nextDate = nextMonth.format(formatter);
-//
-//		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
-//		String currentTime = LocalTime.now().format(timeFormatter);
-//
-//		Thread.sleep(2000);
-//		onboard.clickPickLocation();
-//		Thread.sleep(2000);
-//		onboard.enterPickupDate(todaysDate);
-////		onboard.enterPickupTime(currentTime);
-//		onboard.enterReturnDate(nextDate);
-////		onboard.enterReturnTime(currentTime);
-//		onboard.clickFindCarBtn();
-//	}
+	@Test
+	public void FindCar() throws InterruptedException {
+		FindCarPage onboard = new FindCarPage(driver);
+
+		LocalDate today = LocalDate.now().plusDays(1);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+		String todaysDate = today.format(formatter);
+
+		LocalDate nextMonth = LocalDate.now().plusMonths(1);
+		String nextDate = nextMonth.format(formatter);
+
+		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
+		String currentTime = LocalTime.now().format(timeFormatter);
+
+		Thread.sleep(2000);
+		onboard.clickPickLocation();
+		Thread.sleep(2000);
+		onboard.enterPickupDate(todaysDate);
+//		onboard.enterPickupTime(currentTime);
+		onboard.enterReturnDate(nextDate);
+//		onboard.enterReturnTime(currentTime);
+		onboard.clickFindCarBtn();
+	}
+
 }
